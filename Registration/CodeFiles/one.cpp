0ce94abb23b97681a5a436a7ddff4090c90edcdf
ayushraina <ayushraina@iisc.ac.in>
2024-06-29 03:32:40 +0530

    input files struct read done

--- Registration/CodeFiles/one.cpp
+++ Registration/CodeFiles/one.cpp
@@ -15,32 +15,91 @@ public:
 
 class CoordinateAndIndex {
 public:
-    int sizeOfPatch;
-    vector<vector<double>> coordinates;
+    vector< Point* > coordinates;
     vector<int> indexes;
 
-    // constructor
-    CoordinateAndIndex(int PatchSize) {
-        this->sizeOfPatch = PatchSize;
-        this->coordinates.resize(PatchSize, vector<double>(3, 0));
-        this->indexes.resize(PatchSize, 0);
-    }
-
     // function to add new coordinate
-    void addCoordinate(Point* P, int indexNumber) {
-        this->coordinates[indexNumber][0] = P->x;
-        this->coordinates[indexNumber][1] = P->y;
-        this->coordinates[indexNumber][2] = P->z;
+    void addCoordinate(Point* P) {
+        this->coordinates.push_back(P);
     }
 
     // function to add index
-    void addIndex(int index, int indexNumber) {
-        this->indexes[indexNumber] = index;
+    void addIndex(int index) {
+        this->indexes.push_back(index);
     }
 };
 
 
 
 int main() {
-    
+    vector<  CoordinateAndIndex *  > PatchContent;
+    int numPatches = 8;
+
+    vector<string> coordinatePaths;
+    for(int i = 0;i < numPatches; i++) {
+        string coordPath = "../Groups/group" + to_string(i+1) + ".txt";
+        coordinatePaths.push_back(coordPath);
+    }
+
+    vector<string> indexPaths;
+    for(int i = 0;i < numPatches; i++) {
+        string indexPath = "../Indexes/index" + to_string(i+1) + ".txt";
+        indexPaths.push_back(indexPath);
+    }
+
+    // print coordinate paths
+    cout << "Coordinate Paths" << endl;
+    for(auto path : coordinatePaths) {
+        cout << path << endl;
+    }
+
+    // print index paths
+    cout << "Index Paths" << endl;
+    for(auto path : indexPaths) {
+        cout << path << endl;
+    }
+
+
+    // reading the coordinates and indexes
+    for(int i = 0;i < numPatches; i++) {
+        CoordinateAndIndex* CAI;
+
+        ifstream coordFile(coordinatePaths[i]);
+        ifstream indexFile(indexPaths[i]);
+
+        if(!coordFile.is_open() or !indexFile.is_open()) {
+            cout << "Error in opening the file" << endl;
+            exit(0);
+        }
+        else {
+            cout << "Files are opened successfully" << endl;
+            CAI = new CoordinateAndIndex();
+
+            string line;
+            while(getline(coordFile, line)) {
+                istringstream ss(line);
+                double x, y, z;
+                ss >> x >> y >> z;
+                Point* P = new Point(x, y, z);
+                CAI->addCoordinate(P);
+            }
+
+            while(getline(indexFile, line)) {
+                istringstream ss(line);
+                int index;
+                ss >> index;
+                CAI->addIndex(index);
+            }
+
+            PatchContent.push_back(CAI);
+        }
+
+    }
+
+    // printing the readed coordinates and indexes
+    for(int i = 0;i < numPatches; i++) {
+        cout << i+1 << "th Patch" << endl;
+        cout << "Number of Coordinates: " << PatchContent[i]->coordinates.size() << endl;
+        cout << "Number of Indexes: " << PatchContent[i]->indexes.size() << endl;
+    }
 }
